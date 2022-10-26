7f5ab4b008e78a52668e6d3f8c3bc252fb1f2150
Liam Pilarski <liampilarski@gmail.com>
2022-10-26 16:55:47 -0400

    Update relation.py

--- relation.py
+++ relation.py
@@ -1,87 +1,48 @@
 import pandas as pd
 import numpy as np
-import math
-import statsmodels.tsa.stattools as tools
+
+import time;
 from tardis_test import date
-import matplotlib.pyplot as plt
-
-step = 1
-
-ETH_data = pd.read_csv("{}_ETH-USD.csv".format(date))[::step]
-BTC_data = pd.read_csv("{}_BTC-USD.csv".format(date))[::step]
-
-ETH_ask = ETH_data["ask_price"]
-BTC_ask = BTC_data["ask_price"]
-
-min = pd.Series(ETH_ask/BTC_ask).min()
-max = pd.Series(ETH_ask/BTC_ask).max()
-mean_ = pd.Series(ETH_ask/BTC_ask).mean()
 
 
-ETH_ask = (ETH_ask - ETH_ask.min()) / (ETH_ask.max() - ETH_ask.min())
-BTC_ask = (BTC_ask - BTC_ask.min()) / (BTC_ask.max() - BTC_ask.min())
-
-stationary = pd.Series(ETH_ask/BTC_ask)
-omit = []
-for num, x in enumerate(stationary):
-    if (x == 0 or math.isinf(x)): omit.append(num)
-stationary = stationary.drop(stationary.index[omit]).reset_index(drop=True)
-mean = stationary.mean()
-
-print((mean_ - min)/(max-min))
-print(mean)
-
-date_ = "2022-10-24"
-
-ETH_data = pd.read_csv("{}_ETH-USD.csv".format(date_))[::step].reset_index(drop=True)
-BTC_data = pd.read_csv("{}_BTC-USD.csv".format(date_))[::step].reset_index(drop=True)
+BTC_data = pd.read_csv("datasets/ftx-us_book_ticker_{}_BTC-USD.csv".format(date))
+ETH_data = pd.read_csv("datasets/ftx-us_book_ticker_{}_ETH-USD.csv".format(date))
 
 
-ETH_ask = ETH_data["ask_price"]
-BTC_ask = BTC_data["ask_price"]
+BTC_data['timestamp'] = pd.to_datetime(BTC_data['timestamp'], unit='us').dt.ceil(freq='min')  
+ETH_data['timestamp'] = pd.to_datetime(ETH_data['timestamp'], unit='us').dt.ceil(freq='min')  
+BTC_data['local_timestamp'] = pd.to_datetime(BTC_data['local_timestamp'], unit='us').dt.ceil(freq='min')  
+ETH_data['local_timestamp'] = pd.to_datetime(ETH_data['local_timestamp'], unit='us').dt.ceil(freq='min')  
 
-ETH_ask = (ETH_ask - ETH_ask.min()) / (ETH_ask.max() - ETH_ask.min())
-BTC_ask = (BTC_ask - BTC_ask.min()) / (BTC_ask.max() - BTC_ask.min())
+def clean(df):
+    prev_stamp = None
+    prev_ask = df['ask_price'][0]
+    count = 0
+    omit = []
+    percent_change = []
+    for time_stamp, ask_price in zip(df['timestamp'], df['ask_price']):
+        if time_stamp != prev_stamp:
+            prev_stamp = time_stamp
+            percent_change.append((ask_price - prev_ask) / ask_price)
+            prev_ask = ask_price
+        else:
+            omit.append(count)
+        count += 1
 
+    df = df.drop(omit).reset_index(drop=True)
+    df['local_timestamp'] = pd.Series(percent_change)
+    df = df.rename(columns={'local_timestamp':'%_change'})
+    return df
 
-ratio = pd.Series(ETH_ask/BTC_ask)
-omit = []
-for num, x in enumerate(ratio):
-    if (x == 0 or math.isinf(x)): omit.append(num)
-ratio = ratio.drop(ratio.index[omit]).reset_index(drop=True)
-
-stds = ratio.rolling(30).std()
-
-
-lower_line = mean - stds
-upper_line = mean + stds
+BTC_data = clean(BTC_data)
+ETH_data = clean(ETH_data)
+ETH_data = ETH_data.drop([0])
+print(BTC_data.head())
+print(ETH_data.head())
 
 
 
-for num, std in enumerate(stds):
-    if ratio[num] > mean + std: 
-        print("Greater {}".format(num))
-    elif ratio[num] < mean - std: 
-        print("Less {}".format(num))
-    else: 
-        print("Return {}".format(num))
-
-x = [x + 1 for x in range(len(ratio))]
-
-print(lower_line[-5:])
-print(upper_line[-5:])
-
-plt.plot(x, ratio, label = "line 1")
-plt.plot(x, lower_line, label = "line 2")
-plt.plot(x, upper_line, label = "line 3")
-plt.legend()
-plt.show()
 
 
-
-# print(stationary.rolling(30).std().mean())
-# print(tools.adfuller(ETH_ask))
-# print(tools.adfuller(BTC_ask))
-# print(tools.coint(ETH_ask, BTC_ask))
-# print(tools.adfuller(ETH_ask/BTC_ask))
-
+BTC_data.to_csv('{}_BTC-USD.csv'.format(date))
+ETH_data.to_csv('{}_ETH-USD.csv'.format(date))
