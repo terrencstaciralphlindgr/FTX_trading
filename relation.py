b8d9a59baeefc6d8666f8038eaa4a7d701c1d7ec
Liam Pilarski <liampilarski@gmail.com>
2022-10-25 22:42:23 -0400

    Add pairs trading test

---
+++ relation.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import numpy as np
+import math
+import statsmodels.tsa.stattools as tools
+from tardis_test import date
+import matplotlib.pyplot as plt
+
+step = 1
+
+ETH_data = pd.read_csv("{}_ETH-USD.csv".format(date))[::step]
+BTC_data = pd.read_csv("{}_BTC-USD.csv".format(date))[::step]
+
+ETH_ask = ETH_data["ask_price"]
+BTC_ask = BTC_data["ask_price"]
+
+min = pd.Series(ETH_ask/BTC_ask).min()
+max = pd.Series(ETH_ask/BTC_ask).max()
+mean_ = pd.Series(ETH_ask/BTC_ask).mean()
+
+
+ETH_ask = (ETH_ask - ETH_ask.min()) / (ETH_ask.max() - ETH_ask.min())
+BTC_ask = (BTC_ask - BTC_ask.min()) / (BTC_ask.max() - BTC_ask.min())
+
+stationary = pd.Series(ETH_ask/BTC_ask)
+omit = []
+for num, x in enumerate(stationary):
+    if (x == 0 or math.isinf(x)): omit.append(num)
+stationary = stationary.drop(stationary.index[omit]).reset_index(drop=True)
+mean = stationary.mean()
+
+print((mean_ - min)/(max-min))
+print(mean)
+
+date_ = "2022-10-24"
+
+ETH_data = pd.read_csv("{}_ETH-USD.csv".format(date_))[::step].reset_index(drop=True)
+BTC_data = pd.read_csv("{}_BTC-USD.csv".format(date_))[::step].reset_index(drop=True)
+
+
+ETH_ask = ETH_data["ask_price"]
+BTC_ask = BTC_data["ask_price"]
+
+ETH_ask = (ETH_ask - ETH_ask.min()) / (ETH_ask.max() - ETH_ask.min())
+BTC_ask = (BTC_ask - BTC_ask.min()) / (BTC_ask.max() - BTC_ask.min())
+
+
+ratio = pd.Series(ETH_ask/BTC_ask)
+omit = []
+for num, x in enumerate(ratio):
+    if (x == 0 or math.isinf(x)): omit.append(num)
+ratio = ratio.drop(ratio.index[omit]).reset_index(drop=True)
+
+stds = ratio.rolling(30).std()
+
+
+lower_line = mean - stds
+upper_line = mean + stds
+
+
+
+for num, std in enumerate(stds):
+    if ratio[num] > mean + std: 
+        print("Greater {}".format(num))
+    elif ratio[num] < mean - std: 
+        print("Less {}".format(num))
+    else: 
+        print("Return {}".format(num))
+
+x = [x + 1 for x in range(len(ratio))]
+
+print(lower_line[-5:])
+print(upper_line[-5:])
+
+plt.plot(x, ratio, label = "line 1")
+plt.plot(x, lower_line, label = "line 2")
+plt.plot(x, upper_line, label = "line 3")
+plt.legend()
+plt.show()
+
+
+
+# print(stationary.rolling(30).std().mean())
+# print(tools.adfuller(ETH_ask))
+# print(tools.adfuller(BTC_ask))
+# print(tools.coint(ETH_ask, BTC_ask))
+# print(tools.adfuller(ETH_ask/BTC_ask))
+
--- tardis_test.py
+++ tardis_test.py
@@ -4,44 +4,64 @@
 from tardis_dev import datasets, get_exchange_details
 import logging
 import os
+import pandas as pd
 
-# optionally enable debug logs
-# logging.basicConfig(level=logging.DEBUG)
+date = "2022-10-23"
 
-exchange = 'ftx-us'
-exchange_details = get_exchange_details(exchange)   
+if __name__ == '__main__':
+    exchange = 'ftx-us'
+    exchange_details = get_exchange_details(exchange)   
+    allowed_symbols = ['ETH-USD', 'BTC-USD']
 
-# iterate over and download all data for every symbol
-for symbol in exchange_details["datasets"]["symbols"]:
-    # alternatively specify datatypes explicitly ['trades', 'incremental_book_L2', 'quotes'] etc
-    # see available options https://docs.tardis.dev/downloadable-csv-files#data-types
-    data_types = ['book_ticker'] #symbol["dataTypes"]  
-    symbol_id = symbol["id"]
-    if symbol_id not in ['ETH-USD', 'BTC-USD']: continue
-    from_date = "2022-09-30T00:00:00.000Z" #symbol["availableSince"]
-    to_date = symbol["availableTo"]
-    print(to_date)
 
-    # skip groupped symbols
-    if symbol_id in ['PERPETUALS', 'SPOT', 'FUTURES']:
-        continue
+    for symbol in exchange_details["datasets"]["symbols"]:
+        
+        symbol_id = symbol["id"]
+        if symbol_id not in allowed_symbols: continue
 
-    print(f"Downloading {exchange} {data_types} for {symbol_id} from {from_date} to {to_date}")
+        data_types = ['book_ticker'] #symbol["dataTypes"]  
+        from_date = "{}T00:00:00.000Z".format(date) #symbol["availableSince"]
+        to_date = "{}T00:00:00.000Z".format(date) #symbol["availableTo"]
 
-    # each CSV dataset format is documented at https://docs.tardis.dev/downloadable-csv-files#data-types
-    # see https://docs.tardis.dev/downloadable-csv-files#download-via-client-libraries for full options docs
-    datasets.download(
-        exchange = exchange,
-        data_types = data_types,
-        from_date =  from_date,
-        to_date = to_date,
-        symbols = [symbol_id],
-        # TODO set your API key here
-        api_key = "pyxqIQxeooyMo-6iWms8xfxOtdkeADrPBa3iHj0_",
-        # path where CSV data will be downloaded into
-        download_dir = "./datasets",
-    )
+        print(f"Downloading {exchange} {data_types} for {symbol_id} from {from_date} to {to_date}")
 
-os.chdir("datasets")
-for filename in os.listdir():
-    os.system("gzip -d " + filename)
+        datasets.download(
+            exchange = exchange,
+            data_types = data_types,
+            from_date =  from_date,
+            to_date = to_date,
+            symbols = [symbol_id],
+            api_key = "pyxqIQxeooyMo-6iWms8xfxOtdkeADrPBa3iHj0_",
+            download_dir = "./datasets",
+        )
+
+    os.chdir("datasets")
+    for filename in os.listdir():
+        os.system("gzip -d " + filename)
+
+    os.chdir("../")
+
+    BTC_data = pd.read_csv("datasets/ftx-us_book_ticker_{}_BTC-USD.csv".format(date))
+    ETH_data = pd.read_csv("datasets/ftx-us_book_ticker_{}_ETH-USD.csv".format(date))
+
+
+    BTC_data['timestamp'] = pd.to_datetime(BTC_data['timestamp'], unit='us').dt.ceil(freq='s')  
+    ETH_data['timestamp'] = pd.to_datetime(ETH_data['timestamp'], unit='us').dt.ceil(freq='s')  
+    BTC_data['local_timestamp'] = pd.to_datetime(BTC_data['local_timestamp'], unit='us').dt.ceil(freq='s')  
+    ETH_data['local_timestamp'] = pd.to_datetime(ETH_data['local_timestamp'], unit='us').dt.ceil(freq='s')  
+
+    def clean(df):
+        prev_stamp = None
+        omit = []
+        for num, time_stamp in enumerate(df['timestamp']):
+            if time_stamp != prev_stamp:
+                prev_stamp = time_stamp
+            else:
+                omit.append(num)
+
+        return df.drop(omit).reset_index(drop=True)
+
+    BTC_data = clean(BTC_data)
+    ETH_data = clean(ETH_data)
+
+    print(BTC_data.head())
